c8a3d584dbf9ad0b80d4fab10ab3f81c102339b4
Serah Njoroge <105037236+njoroge-s@users.noreply.github.com>
2023-03-31 14:39:18 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,7 +9,7 @@ def canUnlockAll(boxes):
     for n in range(1, len(boxes) - 1):
         unlocked = False
         for i in range(len(boxes)):
-            unlocked = n in boxes[i] and n!= i
+            unlocked = n in boxes[i] and n != i
             if unlocked:
                 break
         if unlocked is False:
